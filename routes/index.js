a55cb95dde09907278db612b47f9fc085d84d438
Luka Kušec <lk52346@fer.hr>
2022-11-02 01:34:01 +0100

    Dodano sve

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const { Pool } = require('pg')
+const { createHash } = require('crypto');
 
 const pool = new Pool({
     user: process.env.DB_USER,
@@ -23,6 +24,11 @@ async function getKlubGood(ime){
   return result.rows
 }
 
+async function getUser(user){
+  var result = await pool.query("SELECT * FROM users WHERE username LIKE $1", [user]);
+  return result.rows
+}
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -39,5 +45,40 @@ router.get('/sql', async function(req, res) {
   }
   res.render('index', { klubovi: klubovi, onsql: chk, filter: req.query.filter });
 });
+
+router.post('/login', async function(req, res, next) {
+  var user = req.body.username
+  var password = req.body.password
+
+  var chk = req.body.on2 == "on2"
+
+  var userDB = await getUser(user)
+  var poruka = ""
+
+  if (userDB.length > 0){
+    if(hash(password).localeCompare(userDB[0].passwordhash)==0){
+      poruka = "Dobrodošli, " + user
+      user = undefined
+    } else{
+      if(chk){
+        poruka = "Pogrešna šifra za tog korisnika."
+      } else{
+        poruka = "Netočni podaci za prijavu!"
+      }
+    }
+  } else{
+    if (chk){
+      poruka = "Nepostojeći user!"
+    } else{
+      poruka = "Netočni podaci za prijavu!"
+    }
+  }
+
+  res.render('index', { klubovi: {}, poruka: poruka, onba: chk, username: user});
+});
+
+function hash(string) {
+  return createHash('sha256').update(string).digest('hex');
+}
 
 module.exports = router;
